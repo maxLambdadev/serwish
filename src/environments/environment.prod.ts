31189f7a0e26d10eb7374727b3fece17a7b84148
maxLambdadev <berishvilitemoraz11@gmail.com>
2023-08-26 23:59:21 -0700

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8000'
   // apiUrl: 'https://api.serwish.ge'
-  apiUrl: 'https://api.main.serwish.ge'
+  apiUrl: 'http://api.main.serwish.ge'
   // apiUrl: 'https://services.shopgeorgia.ge/public'
 };
